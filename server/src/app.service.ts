9ea4083dbe65e7985b79d2781a63d04c1cf6cc08
naol86 <naol.kasinet@gmail.com>
2024-11-27 08:42:35 +0300

    server side

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello(): Promise<string> {
+    const openai = new OpenAI({
+      apiKey: '',
+      baseURL: 'https://api.x.ai/v1',
+    });
+    console.log('hello world');
+
+    const completion = await openai.chat.completions.create({
+      model: 'grok-beta',
+      messages: [
+        {
+          role: 'system',
+          content:
+            "You are Grok, a chatbot inspired by the Hitchhiker's Guide to the Galaxy.",
+        },
+        {
+          role: 'user',
+          content: 'What is the meaning of life, the universe, and everything?',
+        },
+      ],
+    });
+
+    console.log(completion.choices[0].message);
+
     return 'Hello World!';
   }
 }
